2e2c1fb0d76500dd93284982e05f434908bcd62d
alizs10 <ali.text77@gmail.com>
2023-10-20 00:27:00 +0330

    update: add transition to circular progress bar

--- src/components/CircleProgressBar.jsx
+++ src/components/CircleProgressBar.jsx
@@ -11,7 +11,7 @@ function CircleProgressBar({ themeIndex, percentage }) {
         <div className='relative w-fit'>
             <svg width="60" height="60" viewBox="-25 -25 250 250" version="1.1" xmlns="http://www.w3.org/2000/svg" style={{ transform: "rotate(-90deg)" }}>
                 <circle r="90" cx="100" cy="100" fill="transparent" className='stroke-black/30' strokeWidth="4" strokeDasharray="565.2px" strokeDashoffset="0"></circle>
-                <circle r="90" cx="100" cy="100" className={`progress-bar-theme-${themeIndex}`} strokeWidth="20" strokeLinecap="round" strokeDashoffset={`${strokeDashoffset}px`} fill="transparent" strokeDasharray={`${strokeDasharray}px`}></circle>
+                <circle r="90" cx="100" cy="100" className={`progress-bar-theme-${themeIndex} transition-all duration-300`} strokeWidth="20" strokeLinecap="round" strokeDashoffset={`${strokeDashoffset}px`} fill="transparent" strokeDasharray={`${strokeDasharray}px`}></circle>
             </svg>
 
             <span className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-bold text-xl progress-bar-percentage-theme-${themeIndex}`}>{percentage}</span>
