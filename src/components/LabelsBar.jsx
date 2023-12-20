39b1b5048d8b01cf22c942d23e99268c25f5af5b
alizs10 <ali.text77@gmail.com>
2023-12-20 19:49:11 +0330

    update and improve ui

--- src/components/LabelsBar.jsx
+++ src/components/LabelsBar.jsx
@@ -14,20 +14,20 @@ function LabelsBar({ setNewLabelWindowVis }) {
         <div className='w-full flex flex-nowrap gap-x-3 pt-1 px-3'>
             <div
                 onClick={() => setNewLabelWindowVis(true)}
-                className={`px-3 py-1 h-fit rounded-3xl shadow-md shadow-black bg-gray-700 flex justify-center items-center text-white`}>
+                className={`px-3 py-1 h-fit cursor-pointer rounded-3xl shadow-md shadow-black bg-gray-700 flex justify-center items-center text-white`}>
                 <div className='w-5'>
                     <PlusIcon />
                 </div>
             </div>
 
-            <div className='overflow-x-scroll flex flex-nowrap gap-x-3 pb-3'>
+            <div className='overflow-x-scroll no-scrollbar flex flex-nowrap gap-x-3 pb-3'>
 
 
                 {labels.map(label => (
                     <div
                         key={label._id}
                         onClick={() => handleSelectLabel(label._id)}
-                        className={`px-3 py-1 rounded-3xl ${label._id === selectedLabel ? 'bg-emerald-600' : 'bg-gray-700'} shadow-md shadow-black text-sm text-white`}>{label.name}</div>
+                        className={`px-3 py-1 cursor-pointer rounded-3xl ${label._id === selectedLabel ? 'bg-emerald-600' : 'bg-gray-700'} shadow-md shadow-black text-sm text-white`}>{label.name}</div>
                 ))}
             </div>
 
--- src/components/Rewards/Rewards.jsx
+++ src/components/Rewards/Rewards.jsx
@@ -4,6 +4,7 @@ import Goals from './Goals'
 import NewGoalWindow from './NewGoalWindow'
 import ReceivedPrizes from './ReceivedPrizes'
 import { AnimatePresence } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 function Rewards() {
 
@@ -19,14 +20,19 @@ function Rewards() {
     }
 
     return (
-        <section className='max-w-[600px] mx-auto fixed inset-0 bg-slate-900 z-[999999] overflow-y-scroll'>
+        <motion.section
+            initial={{ x: '100%' }}
+            animate={{ x: '0' }}
+            exit={{ x: '100%' }}
+            transition={{ bounce: "none", duration: .2 }}
+            className='max-w-[600px] mx-auto fixed inset-0 bg-slate-900 z-[999999] overflow-y-scroll'>
             <Header handleOpenNewGoalWindow={handleOpenNewGoalWindow} />
             <Goals />
             <ReceivedPrizes />
             <AnimatePresence>
                 {newGoalWindowVis && (<NewGoalWindow handleClose={handleCloseNewGoalWindow} />)}
             </AnimatePresence>
-        </section>
+        </motion.section>
     )
 }
 
